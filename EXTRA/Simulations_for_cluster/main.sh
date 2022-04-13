d9b924c601f9749dacdd29546670dacc2b20e2aa
alessandrozito <alezitoart@gmail.com>
2022-04-13 23:24:53 +0200

    fixed main

--- EXTRA/Simulations_for_cluster/main.sh
+++ EXTRA/Simulations_for_cluster/main.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --partition=dunsonlab --account=dunsonlab
+#SBATCH --partition=dunsonlab, scavenger --account=dunsonlab
 #SBATCH --output=DPMM.out
 #SBATCH --job-name=DPMM
 #SBATCH --mail-user=az119@duke.edu
-#SBATCH --mail-type=END,FAIL  
-##SBATCH --array=1-5
+#SBATCH --mail-type=END,FAIL
+##SBATCH --array=1-8
 ##SBATCH --mem-per-cpu=10G
-#SBATCH --cpus-per-task=6
+#SBATCH --cpus-per-task=20
 module load R
-R CMD BATCH --no-save main.R out_$SLURM_ARRAY_TASK_ID
+R CMD BATCH --no-save main2.R out_$SLURM_ARRAY_TASK_ID
